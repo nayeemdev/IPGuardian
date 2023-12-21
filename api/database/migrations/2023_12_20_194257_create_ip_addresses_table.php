7edbdd6fe5e0d6d9d5ef3b533086ab3e517fbcdb
nayeemdev <nayeem9812@gmail.com>
2023-12-21 10:56:27 +0600

    Add IpAddress model and migration, and define relationships with User model

---
+++ api/database/migrations/2023_12_20_194257_create_ip_addresses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ip_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->ipAddress('ip_address');
+            $table->string('label', 255);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ip_addresses');
+    }
+};
